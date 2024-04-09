3345c011b9c4c4af9bbd276c3b4135b9e4eec59f
Igor Ruzhilov <iruzhilov@gmail.com>
2024-04-09 14:58:14 +0500

    development: Add

---
+++ app/config/database.py
@@ -0,0 +1,30 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+from app.config.db_config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+from app.models.models import models_metadata
+
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_async_engine(DATABASE_URL)
+
+models_metadata.bind = engine
+
+
+async def create_tables():
+    async with engine.begin() as connection:
+        await connection.run_sync(models_metadata.create_all)
+
+
+async_session_maker = async_sessionmaker(
+    engine=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+
+async def get_async_session():
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
---
+++ app/models/models.py
@@ -0,0 +1,51 @@
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey, CheckConstraint, Enum
+from sqlalchemy.orm import declarative_base, relationship
+
+models_metadata = MetaData()
+Base = declarative_base(metadata=models_metadata)
+
+
+class Patient(Base):
+    __tablename__ = 'users'
+    metadata = models_metadata
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(256), nullable=False)
+    last_name = Column(String(256), nullable=False)
+    middle_name = Column(String(256), nullable=False)
+    IIN = Column(String(12), nullable=False, unique=True)
+    hashed_password = Column(String(1024), nullable=False, default=False)
+    gender = Column(Enum('Male', 'Female', name='genderEnum'), nullable=False, default=False)
+    age = Column(Integer, CheckConstraint('age >= 0 AND age <= 120'), nullable=False)
+
+    doctor_id = Column(Integer, ForeignKey('doctors.id'), nullable=False)
+    doctor = relationship("Doctor", back_populates="patients")
+
+
+class Doctor(Base):
+    __tablename__ = 'doctors'
+    metadata = models_metadata
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(256), nullable=False)
+    last_name = Column(String(256), nullable=False)
+    middle_name = Column(String(256), nullable=False)
+    IIN = Column(String(12), nullable=False, unique=True)
+    hashed_password = Column(String(1024), nullable=False, default=False)
+    gender = Column(Enum('Male', 'Female', name='genderEnum'), nullable=False, default=False)
+    age = Column(Integer, CheckConstraint('age >= 0 AND age <= 120'), nullable=False)
+    qualification = Column(Enum('Pediatrician', 'Gynecologist', 'Psychiatrist', 'Internists',
+                                'Oncologist', 'Dermatologist', name='doctorQualificationEnum'), nullable=False, default=False)
+    patients = relationship("Patient", back_populates="doctor")
+
+
+class Admin(Base):
+    __tablename__ = 'admins'
+    metadata = models_metadata
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(256), nullable=False)
+    last_name = Column(String(256), nullable=False)
+    middle_name = Column(String(256), nullable=False)
+    username = Column(String(12), nullable=False, unique=True)
+    hashed_password = Column(String(1024), nullable=False, default=False)
