498b357d061af225a9b4d505ffe9baf898133b98
Igor Ruzhilov <iruzhilov@gmail.com>
2024-04-11 18:00:07 +0500

    refactoring: Change form data parameters in 'login_for_access_token' method.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.v1.routers.patient_router import router as patient_router
 from app.api.v1.routers.admin_router import router as admin_router
 from app.api.v1.routers.doctor_router import router as doctor_router
 from app.api.v1.auth.auth_router import router as auth_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(patient_router)
 app.include_router(admin_router)
